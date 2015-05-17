d8aeba3702a697884c6cd16434eddfd4b524ade8
cem <jimcc333@gmail.com>
2015-05-17 02:09:25 -0500

    implements the definitions of encrpt decyrpt and test functions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 
 using namespace std;
 
+/* encrypting function */
+string encrypt(string &input){
+	return input;
+}
+
+/* decrypting function */
+string decrypt(string &input){
+	return input;
+}
+
+/* test to see output from decrypt matches input */
+bool test(string &input){
+	string encrypted = encrypt(input);
+	
+	//I dont think this is the right way to test of two strings are same
+	if(input == decrypt(encrypted)){
+		return true;
+	} else {
+		return false;
+	}
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 3){
@@ -12,16 +34,20 @@ int main(int argc, char* argv[])
 		
     cout << "    Inputs: " << argv[1] << " " << argv[2] << endl;
 
-		ifstream input (argv[1]);
-		string line;
+		ifstream inf (argv[1]);
 		
-		if (input.is_open()) {
-    while ( getline (input,line) ) {
-    	cout << line << '\n';
-    }
-    input.close();
-  	}
+		// reads the whole file in input
+		string input((std::istreambuf_iterator<char>(inf)), std::istreambuf_iterator<char>());
 		
+		inf.close();
+		
+		cout << input << endl;
+		
+		if(test(input)){
+			cout << "Passed test!" << endl;
+		} else {
+			cout << "---Failed test" << endl;
+		}		
 
     return 0;
 }
